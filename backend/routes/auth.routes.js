44d779007c33d5f19c5dba5cb2000b4aa084a027
Ridham Vaghasiya <ridhamvaghasiya@03.com>
2026-06-18 10:06:24 +0530

    update routes

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -3,6 +3,6 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
-router.post("/login", loginUser);
+router.post("/login", authController.login);
 
 module.exports = router;
